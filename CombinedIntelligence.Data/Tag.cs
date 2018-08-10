b1e237ce4f4c850ffbcf6f55a829d359ed20dbb8
Nick <nicholas@bbd.co.za>
2018-08-10 20:16:25 +0200

    Added a add method for tags

--- CombinedIntelligence.Data/Tag.cs
+++ CombinedIntelligence.Data/Tag.cs
@@ -6,5 +6,19 @@ namespace CombinedIntelligence.Data
 {
     public class Tag
     {
+        public string Name;
+        public static List<Tag> Tags;
+
+        public void AddTag(Tag tag)
+        {
+            foreach (Tag t in Tags)
+            {
+                if (t.Name == tag.Name)
+                {
+                    return;
+                }
+            }
+            Tags.Add(tag);
+        }
     }
 }
