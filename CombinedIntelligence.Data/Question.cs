28a8089596b9953aa9b6a9b2482b670efcaa1a9f
Azuthedar <Johann.Buys97@gmail.com>
2018-08-11 13:23:11 +0200

    Updated User class

--- CombinedIntelligence.Data/Question.cs
+++ CombinedIntelligence.Data/Question.cs
@@ -3,34 +3,40 @@ using System.Collections.Generic;
 
 namespace CombinedIntelligence.Data
 {
-    public class Question
-    {
-        public int Id { get; }
-        public int UserId { get; set; }
-        public string HeaderText { get; set; }
-        public string BodyText { get; set; }
-        public List<Tag> Tags { get; }
-        public DateTime DatePosted { get; set; }
-        public List<Vote> Votes { get; }
+	public class Question
+	{
+		public int Id { get; }
+		public int UserId { get; set; }
+		public string HeaderText { get; set; }
+		public string BodyText { get; set; }
+		public List<Tag> Tags { get; }
+		public DateTime DatePosted { get; set; }
+		public List<Vote> Votes { get; }
 
-        public int VoteScore
-        {
-            get
-            {
-                int voteScore = 0;
-                foreach (Vote vote in Votes)
-                {
-                    voteScore += (int)vote.vote;
-                }
+		public int VoteScore
+		{
+			get
+			{
+				int voteScore = 0;
+				foreach (Vote vote in Votes)
+				{
+					voteScore += (int)vote.vote;
+				}
 
-                return voteScore;
-            }
-        }
+				return voteScore;
+			}
+		}
 
-        public Question()
-        {
-            Votes = new List<Vote>();
-            Tags = new List<Tag>();
-        }
-    }
+		public Question()
+		{
+			Votes = new List<Vote>();
+			Tags = new List<Tag>();
+		}
+
+		public void addVote(Vote vote)
+		{
+			Votes.Add(vote);
+		}
+	}
+
 }
