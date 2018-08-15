70e1603c5d7ba67ef83c8236f397cdb6fa10545b
Azuthedar <Johann.Buys97@gmail.com>
2018-08-15 09:59:59 +0200

    Updated routing, added some more navigation info, added headers to Forum

--- Combined Intelligence/App_Start/RouteConfig.cs
+++ Combined Intelligence/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace Combined_Intelligence
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+			/*
+			  
 			routes.MapRoute(
 				name: "Profiles",
 				url: "Profiles/{id}",
@@ -37,6 +39,13 @@ namespace Combined_Intelligence
 				defaults: new { controller = "Forum", action = "Index", id = UrlParameter.Optional }
 			);
 
+			*/
+
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Forum", action = "Index", id = UrlParameter.Optional }
+			);
 		}
 	}
 }
