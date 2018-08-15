e8346886ef070dec697373f62574ffbd7cba5214
Amilcar Heera <Amilcar@bbd.co.za>
2018-08-15 20:39:18 +0200

    conflict changes

--- Combined Intelligence/Controllers/UserController.cs
+++ Combined Intelligence/Controllers/UserController.cs
@@ -12,126 +12,126 @@ namespace Combined_Intelligence.Controllers
 	{
         #region MockInfo
         User mockUser = new User("John", "John.Murray@gmail.com", "Actuaris", "IMAGE");
-		List<Question> mockQuestions;
-		List<Answer> mockAnswers;
-		List<Reward> mockRewards;
+        List<Question> mockQuestions;
+        List<Answer> mockAnswers;
+        List<Reward> mockRewards;
 
-		public UserController()
-		{
-			mockUser.AddTag(new Tag("C++"));
-			mockUser.AddTag(new Tag("C#"));
-			mockUser.AddTag(new Tag("R"));
-			mockUser.AddTag(new Tag(".NET"));
-			mockUser.AddTag(new Tag("ASP.NET"));
+        public UserController()
+        {
+            mockUser.AddTag(new Tag("C++"));
+            mockUser.AddTag(new Tag("C#"));
+            mockUser.AddTag(new Tag("R"));
+            mockUser.AddTag(new Tag(".NET"));
+            mockUser.AddTag(new Tag("ASP.NET"));
 
-			mockQuestions = new List<Question>();
+            mockQuestions = new List<Question>();
 
-			List<Tag> mockTags = new List<Tag>();
+            List<Tag> mockTags = new List<Tag>();
 
-			mockTags.Add(new Tag("C#"));
-			mockTags.Add(new Tag("C++"));
-			mockTags.Add(new Tag("ASP.NET"));
+            mockTags.Add(new Tag("C#"));
+            mockTags.Add(new Tag("C++"));
+            mockTags.Add(new Tag("ASP.NET"));
 
-			DateTime date = System.DateTime.Now;
+            DateTime date = System.DateTime.Now;
 
-			mockQuestions.Add(new Question(mockUser.Id, "What is the true meaning of Life?", "LoremIpsumExplanation", mockTags, date));
-			mockQuestions.Add(new Question(mockUser.Id, "What is 2 + 2", "LoremIpsumExplanation", mockTags, date));
-			mockQuestions.Add(new Question(mockUser.Id, "What is mathematics", "LoremIpsumExplanation", mockTags, date));
-			mockQuestions.Add(new Question(mockUser.Id, "Templates", "LoremIpsumExplanation", mockTags, date));
-			mockQuestions.Add(new Question(mockUser.Id, "What is the true meaning of Life?", "LoremIpsumExplanation", mockTags, date));
-			mockQuestions.Add(new Question(mockUser.Id, "What is 2 + 2", "LoremIpsumExplanation", mockTags, date));
-			mockQuestions.Add(new Question(mockUser.Id, "What is mathematics", "LoremIpsumExplanation", mockTags, date));
-			mockQuestions.Add(new Question(mockUser.Id, "Templates", "LoremIpsumExplanation", mockTags, date));
-			mockQuestions.Add(new Question(mockUser.Id, "What is the true meaning of Life?", "LoremIpsumExplanation", mockTags, date));
-			mockQuestions.Add(new Question(mockUser.Id, "What is 2 + 2", "LoremIpsumExplanation", mockTags, date));
-			mockQuestions.Add(new Question(mockUser.Id, "What is mathematics", "LoremIpsumExplanation", mockTags, date));
-			mockQuestions.Add(new Question(mockUser.Id, "Templates", "LoremIpsumExplanation", mockTags, date));
+            mockQuestions.Add(new Question(mockUser.Id, "What is the true meaning of Life?", "LoremIpsumExplanation", mockTags, date));
+            mockQuestions.Add(new Question(mockUser.Id, "What is 2 + 2", "LoremIpsumExplanation", mockTags, date));
+            mockQuestions.Add(new Question(mockUser.Id, "What is mathematics", "LoremIpsumExplanation", mockTags, date));
+            mockQuestions.Add(new Question(mockUser.Id, "Templates", "LoremIpsumExplanation", mockTags, date));
+            mockQuestions.Add(new Question(mockUser.Id, "What is the true meaning of Life?", "LoremIpsumExplanation", mockTags, date));
+            mockQuestions.Add(new Question(mockUser.Id, "What is 2 + 2", "LoremIpsumExplanation", mockTags, date));
+            mockQuestions.Add(new Question(mockUser.Id, "What is mathematics", "LoremIpsumExplanation", mockTags, date));
+            mockQuestions.Add(new Question(mockUser.Id, "Templates", "LoremIpsumExplanation", mockTags, date));
+            mockQuestions.Add(new Question(mockUser.Id, "What is the true meaning of Life?", "LoremIpsumExplanation", mockTags, date));
+            mockQuestions.Add(new Question(mockUser.Id, "What is 2 + 2", "LoremIpsumExplanation", mockTags, date));
+            mockQuestions.Add(new Question(mockUser.Id, "What is mathematics", "LoremIpsumExplanation", mockTags, date));
+            mockQuestions.Add(new Question(mockUser.Id, "Templates", "LoremIpsumExplanation", mockTags, date));
 
-			Random amountOfViewers = new Random();
+            Random amountOfViewers = new Random();
 
-			mockQuestions.ForEach(question =>
-			{
-				int people = amountOfViewers.Next(500, 4000);
-				Random newVoteType = new Random();
-				for (int userId = 0; userId < people; userId++)
-				{
-					int val = newVoteType.Next(-1, 2);
-					question.UpdateVote(new Vote(userId, (VoteTypes)val));
+            mockQuestions.ForEach(question =>
+            {
+                int people = amountOfViewers.Next(500, 4000);
+                Random newVoteType = new Random();
+                for (int userId = 0; userId < people; userId++)
+                {
+                    int val = newVoteType.Next(-1, 2);
+                    question.UpdateVote(new Vote(userId, (VoteTypes)val));
 
-				}
-			});
-
-
-
-			mockAnswers = new List<Answer>();
-
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-			mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
-
-			mockAnswers.ForEach(answer =>
-			{
-				int people = amountOfViewers.Next(500, 1000);
-				Random newVoteType = new Random();
-				for (int userId = 0; userId < people; userId++)
-				{
-					int val = newVoteType.Next(-1, 2);
-					answer.UpdateVote(new Vote(userId, (VoteTypes)val));
-				}
-			});
+                }
+            });
 
 
-			mockRewards = new List<Reward>();
 
-			mockRewards.Add(new Reward("PS4", 500));
-			mockRewards.Add(new Reward("Free Lunch for a Week", 10));
-			mockRewards.Add(new Reward("Some Reward", 99999));
-			mockRewards.Add(new Reward("The Speaker", 100000));
-			mockRewards.Add(new Reward("The Watcher", 500000));
-			mockRewards.Add(new Reward("Smartwatch", 250));
+            mockAnswers = new List<Answer>();
 
-		}
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
+            mockAnswers.Add(new Answer(1, mockUser.Id, "LoremIpsumAnswer", date));
 
-		#endregion
-		// GET: User
-		public ActionResult Index()
-		{
-			return View();
-		}
+            mockAnswers.ForEach(answer =>
+            {
+                int people = amountOfViewers.Next(500, 1000);
+                Random newVoteType = new Random();
+                for (int userId = 0; userId < people; userId++)
+                {
+                    int val = newVoteType.Next(-1, 2);
+                    answer.UpdateVote(new Vote(userId, (VoteTypes)val));
+                }
+            });
 
-		public ActionResult Profile(int ID)
-		{
-            //mockUser = getUser(ID);
-			ViewBag.ID = mockUser.Id;
-			ViewBag.userName = mockUser.Name;
-			ViewBag.email = mockUser.Email;
-			ViewBag.team = mockUser.Team;
 
-			ViewBag.score = mockUser.Score;
-			ViewBag.Preferences = mockUser.Tags;
+            mockRewards = new List<Reward>();
 
-			ViewBag.Questions = mockQuestions;
-			ViewBag.Answers = mockAnswers;
+            mockRewards.Add(new Reward("PS4", 500));
+            mockRewards.Add(new Reward("Free Lunch for a Week", 10));
+            mockRewards.Add(new Reward("Some Reward", 99999));
+            mockRewards.Add(new Reward("The Speaker", 100000));
+            mockRewards.Add(new Reward("The Watcher", 500000));
+            mockRewards.Add(new Reward("Smartwatch", 250));
 
-			ViewBag.Rewards = mockRewards;
+        }
 
-			return View();
-		}
-		public ActionResult Answers(int ID)
+        #endregion
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Profile(int ID)
+        {
+            ViewBag.ID = mockUser.Id;
+            ViewBag.userName = mockUser.Name;
+            ViewBag.email = mockUser.Email;
+            ViewBag.team = mockUser.Team;
+
+            ViewBag.score = mockUser.Score;
+            ViewBag.Preferences = mockUser.Tags;
+
+            ViewBag.Questions = mockQuestions;
+            ViewBag.Answers = mockAnswers;
+
+            ViewBag.Rewards = mockRewards;
+
+            return View();
+        }
+
+        public ActionResult Answers(int ID)
 		{
             List<Answer> ansList = new List<Answer>();
             using (CombinedIntelligenceAPI.Models.CombinedIntelligenceEntities CI = new CombinedIntelligenceAPI.Models.CombinedIntelligenceEntities())
